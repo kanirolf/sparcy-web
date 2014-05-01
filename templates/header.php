efcd8538527e97fd9c74015dd247b0a3a6c69ea1
kanirolf <wdomingo@uci.edu>
2014-05-01 16:20:57 -0700

    Move nav stylesheet from page-specific stylesheet into template.

--- templates/header.php
+++ templates/header.php
@@ -1,6 +1,11 @@
 <meta charset="UTF-8">
+
 <title>SpArcFiRe Web Interface</title>
+
 <link href='http://fonts.googleapis.com/css?family=Dosis|Concert+One' rel='stylesheet' type='text/css'>
+
+<link rel="stylesheet" href="/templates/nav-style.css"/>
 <link rel="stylesheet" href="style.css" type="text/css">
+
 <script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
 <script src="script.js"></script>
--- templates/nav.php
+++ templates/nav.php
@@ -1,16 +1,18 @@
-<header>
-	<p>SpArcFire <b>WebUI</b></p>
-</header>
-<nav id="steps">
-	<span class="query active">
-		create new query
-	</span>
-	&gt;
-	<span class="processing">
-		process
-	</span>
-	&gt;
-	<span class="results">
-		results
-	</span>
-</nav>
+<div id="sparcfire-top-nav">
+	<header>
+		<p>SpArcFire <b>WebUI</b></p>
+	</header>
+	<nav id="steps">
+		<span class="query active">
+			create new query
+		</span>
+		&gt;
+		<span class="processing">
+			process
+		</span>
+		&gt;
+		<span class="results">
+			results
+		</span>
+	</nav>
+</div>
