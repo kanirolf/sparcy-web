f963802572fc9d17977940a80b1955e171974d68
kanirolf <wdomingo@uci.edu>
2014-05-01 15:04:20 -0700

    Added $_TEMPLATE variable for better access to html templates (or CSS templates, or what have you..)

--- php/includes.php
+++ php/includes.php
@@ -1,7 +1,12 @@
 <?php
 $_ROOT = $_SERVER["DOCUMENT_ROOT"];
+$_TEMPLATES = $_ROOT.'/templates';
 
 include 'functions.php';
-include 'log.php';
+
+// run sessions before logging, in case you wanted to have log based on session vars.
 include 'sessions.php';
+
+include 'log.php';
+
 ?>
