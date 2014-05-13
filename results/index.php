b5bb96aa98fb25a72beee3c5914ab4ca751b43aa
kanirolf <wdomingo@uci.edu>
2014-05-12 21:44:28 -0700

    Clean up results.php.

--- results/index.php
+++ results/index.php
@@ -1,51 +1,4 @@
-<?php 
-
-include $_SERVER["DOCUMENT_ROOT"].'/php/includes.php' 
-
-	/* create a zip for the files. the zip will be created in /outDir
-	 * as galaxy_[imageId]_data.zip with two folders inside: images/
-	 * for the images and tables/ for the comma/tab separated value
-	 * tables.
-	 */
-	
-	$zipName = "galaxy_".$imageData["name"]."_data.zip";
-	$files = new ZipArchive;
-	
-	$files->open($imageData["outDir"]."/".$zip_name, ZipArchive::CREATE);
-	
-	$files->addEmptyDir("images");
-	$files->addEmptyDir("tables");
-	
-	$files->addPattern("/[^.]+\.[ct]sv/", $imageData["outDir"], array(
-			"remove_all_path" => TRUE, "add_path" => "tables/"));
-	
-	$files->addPattern("/[^.]+\.png/", $imageData["outDir"]."/".$imageData["name"], array(
-			"remove_all_path" => TRUE, "add_path" => "images/"));
-	
-	$files->close();
-	
-	// generate an array of <img /> using each image in the output directory
-	
-	$images = array();
-	
-	foreach(preg_grep("/[^.]+\.png/", scandir($imageData["outDir"].'/'.$imageData['name'])) as $image){
-		$suffix = array();
-		preg_match('/[^-]+-[A-Z]?_+([^.]+)\.png/', $image, $suffix);
-		$images[$suffix[1]] = '<img src=\'/process/'.$imageData["id"].'/outDir/'.$imageData["name"].'/'.$image.'\' />';
-	}
-	
-	returnProcessingState(true, "Image successfully processed.", 
-		array(
-			"image" => $imageData["name"].".".$imageData["ext"],
-			"images" => $images,
-			"zip_file" => '/process/'.$imageData["id"].'/outDir/'.$zipName,
-			"command" => $optionString
-	));
-}
-
-?>
-
-
+<?php include $_SERVER["DOCUMENT_ROOT"].'/php/includes.php';?>
 <!DOCTYPE html>
 <html>
 	<head>
@@ -53,16 +6,28 @@ include $_SERVER["DOCUMENT_ROOT"].'/php/includes.php'
 	</head>
 	<body>
 		<?php include $_TEMPLATES."/nav.php" ?>
-		<div id="content">
+		<div class="content result">
 			<div class="container">
+				<?php 
+					if(file_exists($_ROOT."/process/".$_GET["query"]."/info.json")){
+						$galaxyDetails = json_decode(file_get_contents("/process/".$_GET["query"]."/info.json"));
+				?>
 				<header>
 					Results of processing:
 				</header>
-				<a id="download" href="">
+				<a id="download" href="<?php echo $galaxyDetails->zip ?>">
 					download results
 				</a>
 				<div id="resultImages">
+					<?php foreach($galaxyDetails->images as $name => $image){
+						echo '<figure class="resultImage">
+							<img src="'.$image.'"/>
+							<figcaption>'.$name.'</figcaption>
+						</figure>';
+					}?>
 				</div>
+				<?php } else if ($_GET["query"] != "") echo "No such result ID exists. Sorry."; 
+						else echo "Sorry, but a result ID is needed to view a result."?>
 			</div>
 		</div>
 	</body>
